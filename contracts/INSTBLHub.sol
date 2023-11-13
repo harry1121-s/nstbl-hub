89bcdb661e95650dffe140f21e89930dabad6f58
MrAngad <angad.97@protonmail.com>
2023-11-13 09:54:02 +0530

    interface

--- contracts/INSTBLHub.sol
+++ contracts/INSTBLHub.sol
@@ -39,20 +39,44 @@ interface INSTBLHub {
      * @param _user The address of the user
      * @param _amount The amount of tokens to be staked
      * @param _trancheId The tranche id of the user
-     * @param _destAddress The address of the destination
+     * @param _destAddress The address of the destination where the staked tokens will be stored
      */
     function stake(address _user, uint256 _amount, uint8 _trancheId, address _destAddress) external;
     
     /** 
+     * @dev The asset will get allocated
+     * @param _asset The address of the asset
+     * @param _allocation The allocation amount of the asset
     */
     function updateAssetAllocation(address _asset, uint256 _allocation) external;
 
+    /**
+     * @dev The asset feeds will get updated
+     * @param _assetFeeds The array of asset feeds
+     */
     function updateAssetFeeds(address[3] memory _assetFeeds) external;
 
+    /**
+     * @dev The system parameters will get updated
+     * @param _dt The dt value is set
+     * @param _ub The upper bound value is set
+     * @param _lb The lower bound value is set
+     * @param _liquidPercent The liquid percent value is set
+     * @param _tBillPercent The tBill percent value is set
+     */
     function setSystemParams(uint256 _dt, uint256 _ub, uint256 _lb, uint256 _liquidPercent, uint256 _tBillPercent) external;
 
+    /**
+     * @dev The process to redeem the TBills and update the balance
+     * @return usdcRedeemed The amount of USDC redeemed
+     */
     function processTBillWithdraw() external  returns(uint256 usdcRedeemed); 
 
+    /**
+     * @dev The process to retrieve the funds from the contract
+     * @param _asset The address of the asset
+     * @param _amount The amount of the asset to be retrieved
+     */
     function retrieveFunds(address _asset, uint256 _amount) external;
 
 }
