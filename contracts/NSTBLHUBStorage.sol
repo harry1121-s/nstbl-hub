fe7bd8217e277b11534376f3f3caaff161306142
Harshit Singhal <82471455+harry1121-s@users.noreply.github.com>
2023-12-12 03:14:40 +0530

    minor

--- contracts/NSTBLHUBStorage.sol
+++ contracts/NSTBLHUBStorage.sol
@@ -10,13 +10,22 @@ import "./interfaces/INSTBLHub.sol";
 
 contract NSTBLHUBStorage {
 
-    uint256 internal constant REVISION = 1;
-
-    /*//////////////////////////////////////////////////////////////
-                                STORAGE
+     /*//////////////////////////////////////////////////////////////
+    IMMUTABLES & CONSTANTS
     //////////////////////////////////////////////////////////////*/
 
-    uint256 versionSlot;
+    uint256 internal constant REVISION = 1;
+
+    address public immutable USDC = address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    address public immutable USDT = address(0xdAC17F958D2ee523a2206206994597C13D831ec7);
+    address public immutable DAI = address(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+
+
+    /*//////////////////////////////////////////////////////////////
+    State Variables
+    //////////////////////////////////////////////////////////////*/
+
+    uint256 public versionSlot;
     address public nealthyAddr;
     address public atvl;
     address public loanManager;
@@ -31,18 +40,7 @@ contract NSTBLHUBStorage {
 
     mapping(address => uint256) public stablesBalances;
 
-    address public immutable USDC = address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
-    address public immutable USDT = address(0xdAC17F958D2ee523a2206206994597C13D831ec7);
-    address public immutable DAI = address(0x6B175474E89094C44Da98b954EedeAC495271d0F);
-
-    //usdc, usdt, dai
     address[3] public assets;
-    //usdc, usdt, dai
-    // address[3] public assetFeeds = [
-    //     0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6,
-    //     0x3E7d1eAB13ad0104d2750B8863b489D65364e32D,
-    //     0xAed0c38402a5d19df6E4c03F4E2DceD6e29c1ee9
-    // ];
 
     address[3] public assetFeeds;
     uint256 public dt;
@@ -51,7 +49,9 @@ contract NSTBLHUBStorage {
     uint256 public eqTh;
     uint256 public precision;
 
-    uint256 public tBillPercent; // to be removed: TBD
+    uint256 public tBillPercent;
 
-    uint256 [20] __gap;
+    //add new variables here to extended the storage
+    //reduce the gap size equal the size of new variables: to maintain original layout and prevent collision
+    uint256 [26] __gap;
 }
